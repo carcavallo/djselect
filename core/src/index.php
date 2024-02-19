5b4b2d84ca09a7dc0f217078747c221ba310d84e
Alessio Carcavallo <alessio@alessio.fm>
2024-02-19 13:37:06 +0100

    fix: cors

--- core/src/index.php
+++ core/src/index.php
@@ -33,6 +33,14 @@ if (!empty($request_body)) {
 }
 
 header("Content-Type: application/json");
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit();
+}
+
 date_default_timezone_set("Europe/Zurich");
 
 require_once __DIR__ . "/autoload.php";
@@ -57,7 +65,10 @@ $router->setBasePath("/api");
 
 $router->set404("IOController@show404");
 
-$router->before("GET|POST|PUT|DELETE", "/user*", "AuthController@checkLogin");
+$router->before("GET|POST|PUT|DELETE", "/profiles*", "AuthController@checkLogin");
+$router->before("GET|POST|PUT|DELETE", "/events*", "AuthController@checkLogin");
+$router->before("GET|POST|PUT|DELETE", "/bookings*", "AuthController@checkLogin");
+
 
 $router->mount("/auth", function () use ($router) {
     $router->post("/login", "AuthController@login");
