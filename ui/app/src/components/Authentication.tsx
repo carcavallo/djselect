a62724f77d1479e9fe03a701e25372d78b9e1a28
Alessio Carcavallo <alessio@alessio.fm>
2024-02-26 09:21:11 +0100

    fix: fix

--- ui/app/src/components/Authentication.tsx
+++ ui/app/src/components/Authentication.tsx
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
-import { useUserRole } from './useUserRole';
 import Login from './forms/Login';
 import Register from './forms/Register';
 
 
 const Authentication: React.FC = () => {
-  const { role } = useUserRole();
   const [isLogin, setIsLogin] = useState(true);
 
   const toggleForm = () => setIsLogin(!isLogin);
 
   return (
     <>
-      {role && isLogin ? <Login onToggle={toggleForm} /> : <Register onToggle={toggleForm} />}
+      {isLogin ? <Login onToggle={toggleForm} /> : <Register onToggle={toggleForm} />}
     </>
   );
 };
