6523382068267c490b92071c4b6c307521619efd
Alessio Carcavallo <alessio@alessio.fm>
2024-02-27 01:03:24 +0100

    test

--- ui/app/src/components/forms/PasswordResetRequest.tsx
+++ ui/app/src/components/forms/PasswordResetRequest.tsx
@@ -9,7 +9,7 @@ const PasswordResetRequest: React.FC = () => {
     e.preventDefault();
     
     try {
-      const response = await fetch('http://localhost:80/api/auth/reset', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/reset`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email }),
--- ui/app/src/components/forms/SetNewPassword.tsx
+++ ui/app/src/components/forms/SetNewPassword.tsx
@@ -23,7 +23,7 @@ const SetNewPassword: React.FC = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:80/api/auth/reset/confirm', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/reset/confirm`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ token, password }),
